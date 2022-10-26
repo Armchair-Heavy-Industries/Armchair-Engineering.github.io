28d9a04151b9e32151c4ae809587c1fb6a6026f6
Franklyn Tackitt <git@frank.af>
2022-10-26 09:24:43 -0700

    Random videos for now

---
+++ random_video.js
@@ -0,0 +1,22 @@
+var videos = ["bedspin", "printing", "vasing_big", "vasing_small"];
+
+window.addEventListener("DOMContentLoaded", function () {
+  var videoTag = document.getElementById("prexz");
+
+  videoTag.addEventListener("click", (e) => e.preventDefault());
+
+  function createSource(video, mimetype) {
+    var source = document.createElement("source");
+    source.src = video;
+    source.mimetype = mimetype;
+    return source;
+  }
+
+  function randomVideo() {
+    var video = videos[Math.floor(Math.random() * videos.length)];
+
+    videoTag.src = "videos/" + video + ".webm";
+  }
+  videoTag.addEventListener("ended", randomVideo);
+  randomVideo();
+});

137060f0c748ab3e6ad4ba39ecedcc8c4808c03b
Derpimus <lraithel15133@gmail.com>
2023-07-10 01:13:45 -0500

    dasdas

--- redacted/script.js
+++ redacted/script.js
@@ -1,51 +1,47 @@
 function loadHotends() {
   var hotendDropdown = document.getElementById("hotendDropdown");
-  hotendDropdown.disabled = true;
 
-  // Make an API call or fetch data from the "hotends" folder
-  // Populate hotendDropdown with the folder names
 
-  // Example:
-  var hotendOptions = ["Hotend 1", "Hotend 2", "Hotend 3"];
-
+  var hotendOptions = ["Hotend 1", "Hotend 2", "Hotend 3"];    //fetch folder names later
+ 
   hotendOptions.forEach(function (option) {
     var hotendOption = document.createElement("option");
     hotendOption.text = option;
     hotendDropdown.add(hotendOption);
   });
 
-  hotendDropdown.disabled = false;
 }
 
 function loadExtruders() {
   var hotendDropdown = document.getElementById("hotendDropdown");
   var extruderDropdown = document.getElementById("extruderDropdown");
-  extruderDropdown.disabled = true;
 
   var selectedHotend = hotendDropdown.value;
 
-  // Make an API call or fetch data from the "extruders" folder based on the selected hotend
-  // Populate extruderDropdown with the folder names
+  var extruderOptions = [selectedHotend + " Extruder 1", selectedHotend + " Extruder 2", selectedHotend + " Extruder 3"];   //fetch folder names later
 
-  // Example:
-  var extruderOptions = ["Extruder 1", "Extruder 2", "Extruder 3"];
-
-  extruderDropdown.length = 1; // Clear existing options
+  extruderDropdown.length = 1;
   extruderOptions.forEach(function (option) {
     var extruderOption = document.createElement("option");
     extruderOption.text = option;
     extruderDropdown.add(extruderOption);
   });
 
-  extruderDropdown.disabled = false;
 }
 
 function loadProbes() {
+  var hotendDropdown = document.getElementById("hotendDropdown");
   var extruderDropdown = document.getElementById("extruderDropdown");
   var probeDropdown = document.getElementById("probeDropdown");
-  probeDropdown.disabled = true;
-
+  
+  var selectedHotend = hotendDropdown.value;
   var selectedExtruder = extruderDropdown.value;
-  var probeOptions = ["Probe 1", "Probe 2", "Probe 3"];
+  var extruderOptions = [selectedHotend + selectedExtruder + " Probe 1", selectedHotend + selectedExtruder + " Probe 2", selectedHotend + selectedExtruder + " Probe 3"];   //fetch folder names later
+  
+  probeDropdown.length = 1;
+  probeOptions.forEach(function (option) {
+    var probeOption = document.createElement("option");
+    probeOption.text = option;
+    probeDropdown.add(probeOption);
   // Make an API call or fetch data from the "probes" folder based on the selected extruder
   // Populate

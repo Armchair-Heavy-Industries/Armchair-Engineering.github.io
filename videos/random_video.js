5dc7f9bed4bbd4e699ad231b66201d8708ad6a57
Derpimus - Liam Raithel <76272488+lraithel15133@users.noreply.github.com>
2024-02-06 19:05:49 -0600

    Update random_video.js

--- videos/random_video.js
+++ videos/random_video.js
@@ -1,4 +1,4 @@
-var videos = ["bedspin", "printing", "vasing_big", "vasing_small", "throw1", "throw2", "2023-05-04_00-09-20", "2023-06-08_23-30-36", "PXL_20230706_175035756", "PXL_20240118_141605700.TS1", "leddy"];
+var videos = ["bedspin", "printing", "vasing_big", "vasing_small", "throw1", "throw2", "2023-05-04_00-09-20", "2023-06-08_23-30-36", "PXL_20230706_175035756", "PXL_20240118_141605700.TS1"];
 
 window.addEventListener("DOMContentLoaded", function () {
   var videoTag = document.getElementById("prexz");
@@ -21,12 +21,10 @@ window.addEventListener("DOMContentLoaded", function () {
 
     var webmSource = createSource(`videos/${video}.webm`, 'video/webm');
     var mp4Source = createSource(`videos/${video}.mp4`, 'video/mp4');
-    var gifSource = createSource(`videos/${video}.gif`, 'video/gif');
     
 
     videoTag.appendChild(webmSource);
     videoTag.appendChild(mp4Source);
-    videoTag.appendChild(gifSource);
     
     videoTag.load(); // Important to reload the video tag to apply new sources
   }
